8ef53db2ec681e01bc78812cb40a592cc1302a34
liuuu <57059329@qq.com>
2024-01-26 10:57:17 +0800

    feat: init

--- src/app/explore/header.tsx
+++ src/app/explore/header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
 
     return (
         <div className="w-full flex-none fixed z-10 h-14 bg-blue-700 xl:h-18 xl:h-20">
-            Head
+            Header
         </div>
     )
 }
